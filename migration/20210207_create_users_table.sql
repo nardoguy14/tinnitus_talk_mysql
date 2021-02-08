b82e1cc45c782de57f71855bea2ce786abbb4e8a
nardoguy14 <nardoarevalo@me.com>
2021-02-07 22:42:15 -0800

    First commit of mysql migrations

---
+++ migration/20210207_create_users_table.sql
@@ -0,0 +1,10 @@
+use tinnitus_talks;
+
+create table users (
+    username   VARCHAR(64),
+    first_name VARCHAR(64),
+    last_name  VARCHAR(64),
+    email      VARCHAR(64),
+    description longtext,
+    password   binary
+);
---
+++ tables/users.sql
@@ -0,0 +1,12 @@
+use tinnitus_talks;
+
+create table users (
+    username   VARCHAR(64),
+    first_name VARCHAR(64),
+    last_name  VARCHAR(64),
+    email      VARCHAR(64),
+    description longtext,
+    password   binary
+);
+
+drop table users;
