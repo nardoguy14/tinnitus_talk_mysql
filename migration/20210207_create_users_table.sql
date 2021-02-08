ee3e9aeeeb5c20db22720833bbc0cc9f19d9ba1c
nardoguy14 <nardoarevalo@me.com>
2021-02-08 00:14:48 -0800

    Changes to user schema

--- migration/20210207_create_users_table.sql
+++ migration/20210207_create_users_table.sql
@@ -1,6 +1,7 @@
 use tinnitus_talks;
 
 create table users (
+    id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username   VARCHAR(64),
     first_name VARCHAR(64),
     last_name  VARCHAR(64),
