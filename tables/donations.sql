f1c399d8ef11e6f4975502c0e650905edc912ac6
nardoguy14 <nardoarevalo@me.com>
2021-02-22 19:18:48 -0800

    Renaming columns

--- tables/donations.sql
+++ tables/donations.sql
@@ -2,7 +2,7 @@ use tinnitus_talks;
 
 create table donations (
     id               INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username         VARCHAR(64),
+    user_id          VARCHAR(64),
     donor_first_name VARCHAR(64),
     donor_last_name  VARCHAR(64),
     donor_comment    longtext,
